781591fc5ec8497f72b4faa9ff8280689ee0295c
Santosh Gyawali <123115960+isantoshgyawali@users.noreply.github.com>
2023-12-28 10:49:20 +0545

    from-browser-update

--- .config/nvim/lua/kosnate/remap.lua
+++ .config/nvim/lua/kosnate/remap.lua
@@ -6,12 +6,12 @@ vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 -- PackerSync
 vim.keymap.set("n", "<leader>ps", vim.cmd.PackerSync)
 
--- Use vim.keymap.set for the following mappings
+-- Mappings / vertical_motions
 vim.keymap.set("n", "<C-d>", "<C-d>zz") -- half-page-down with cursor-center
 vim.keymap.set("n", "<C-u>", "<C-u>zz") -- half-page-up with cursor-center
 
-vim.keymap.set("n", "<C-f>", "<C-f>zz") -- half-page-down with cursor-center
-vim.keymap.set("n", "<C-b>", "<C-b>zz") -- half-page-up with cursor-center
+vim.keymap.set("n", "<C-f>", "<C-f>zz") -- full-page-down with cursor-center
+vim.keymap.set("n", "<C-b>", "<C-b>zz") -- full-page-up with cursor-center
 
 -- line Numbers 
 vim.o.number = true
