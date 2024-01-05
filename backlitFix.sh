3798634a5e33eda31068638f74156d3dcf2963a1
cosnate <isantoshgyawali@gmail.com>
2024-01-05 19:51:51 +0545

    @backlitFix-2

--- backlitFix.sh
+++ backlitFix.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Before Implementing script give permissions to write 
-# sudo chmod 777 /sys/class/leds/input29::scrolllock/brightness
+# sudo chmod 777 /sys/class/leds/input7::scrolllock/brightness
 
-esc_state=$(cat /sys/class/leds/input29::scrolllock/brightness)
+esc_state=$(cat /sys/class/leds/input7::scrolllock/brightness)
 
 if [[ "$esc_state" == "1" ]]; then 
-	echo 0 >> /sys/class/leds/input29::scrolllock/brightness
+	echo 0 >> /sys/class/leds/input7::scrolllock/brightness
 
 else 
-	echo 1 >> /sys/class/leds/input29::scrolllock/brightness
+	echo 1 >> /sys/class/leds/input7::scrolllock/brightness
 fi
