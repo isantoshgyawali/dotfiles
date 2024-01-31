440cbba110e749792918562463e429f5c7c83c75
cosnate <isantoshgyawali@gmail.com>
2024-01-31 23:57:14 +0545

    upd/aliases-1.4

--- .config/aliases.sh
+++ .config/aliases.sh
@@ -8,19 +8,19 @@ function url(){
 #-------------------
 #  general_aliases  |
 #-------------------
-alias ll='ls -al'
+alias ll='ls -al --classify'
 alias upd='sudo dnf update --refresh && sudo dnf upgrade --refresh'
 alias br='firefox & disown'
 alias gpt='xdg-open https://chat.openai.com & disown'
-alias bing='firefox --private-window bing.com/chat && disown'
+alias bing='firefox --private-window https://www.bing.com/chat && disown'
 
 alias t='tmux'
 alias c='code'
 alias vi='nvim'
-alias svi='sudo nvim'
+alias svi='sudo nvim'  
 
 alias yt='xdg-open https://youtube.com/ & disown'
-alias ytd='youtube-dl'
+alias ytdown='youtube-dl'
 alias tput='trash-put'
 alias tls='trash-list'
 alias tres='trash-restore'
@@ -32,14 +32,22 @@ alias df='duf'
 
 alias e='exit'
 
-alias fcp='xsel --clipboard < $(fzf)'
+alias con='source $HOME/con'
+alias key='xsel --clipboard < $HOME/key.txt'
+alias fcp='xsel --clipboard < $(find ~ -type f | fzf)'
+
 alias pwc='pwd | xsel --clipboard'
 
-#-------------
-#  dir/files  |
-#-------------
-alias open='vi $( find $HOME -type f | fzf --preview "bat --color always {}")'
-alias goto='cd $(find $HOME -type d | fzf) | ll; printf "\033c" && pwd'
+#tgpt 
+alias tco='tgpt -c'
+alias tim='tgpt -img'
+alias tt='tgpt -i'
+
+#-----------------------
+#  dir/files navigate   |
+#------------ ----------
+alias open='vi `find $HOME -type f | fzf --height 50% --reverse --exact --preview "bat --color always {}"`'
+alias goto='cd `find $HOME -type d | fzf --height 50% --reverse --exact`'
 
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -54,11 +62,19 @@ alias px='ps aux | fzf'
 #-------------
 #  git_quick  |
 #-------------
-alias isan='xdg-open https://github.com/isantoshgyawali/ & disown && exit'
 alias ga='git add .'
 alias gc='git commit -m'
-alias gs='git status'
+alias gs='git status --short'
+
+alias gco='git checkout'
 alias gcob='git checkout -b'
-alias gc='git checkout'
-alias gbv='git branch -v'
+
+alias gb='git branch -v'
+alias gba='git branch -avv'
+
 alias gl='git log'
+alias gla='git log -a --decorate --oneline --graph'
+alias glf='git log -n 5 --decorate --oneline --graph'
+alias gcl='key && git clone'
+alias gp='key && git push'
+
