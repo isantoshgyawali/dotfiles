62b042f2dd826448b44dfac20fcb27162c883b3b
Santosh Gyawali <isantoshgyawali@gmail.com>
2024-08-12 16:26:49 +0545

    hypr/utils - wlogout

--- config/hypr/scripts/fullss.sh
+++ config/hypr/scripts/fullss.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-grim $HOME/Pictures/Screenshots/$(date +'%s_grim.png') 
+grim $HOME/Pictures/Screenshots/$(date +'%s_grim.png') | wl-clipboard
 mpv $HOME/.config/hypr/sounds/screenshot.ogg 
