5a5997fc97120b4f29c787f2e35741d55f753f66
Santosh Gyawali <isantoshgyawali@gmail.com>
2025-12-17 06:17:41 +0545

    upd: copy ss

--- config/hypr/scripts/fullss.sh
+++ config/hypr/scripts/fullss.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-grim $HOME/Pictures/Screenshots/$(date +'%s_grim.png')
+FILE="$HOME/Pictures/Screenshots/$(date +'%s_grim.png')"
+grim $FILE && wl-copy < "$FILE"
 mpv $HOME/.config/hypr/sounds/screenshot.ogg 
 notify-send -t 1000 "screenshot captured!"
--- config/hypr/scripts/rectss.sh
+++ config/hypr/scripts/rectss.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-grim -g "$(slurp -o)" $HOME/Pictures/Screenshots/$(date +'%s_grim.png') | wl-clipboard
+FILE="$HOME/Pictures/Screenshots/$(date +'%s_grim.png')"
+grim -g "$(slurp -o)" "$FILE" && wl-copy < "$FILE"
 mpv ~/.config/hypr/sounds/screenshot.ogg
 notify-send -t 1000 "screenshot captured!"
