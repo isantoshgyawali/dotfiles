eef25e042c01dd0a8341809ceb09345f2459f10d
Santosh Gyawali <isantoshgyawali@gmail.com>
2026-03-16 20:11:44 +0545

    blue filter settings

--- config/hypr/scripts/blue.sh
+++ config/hypr/scripts/blue.sh
@@ -3,7 +3,7 @@
 STATE_FILE="/tmp/blue_filter_state"
 if [[ -f "$STATE_FILE" ]]; then
     rm "$STATE_FILE"
-    brightnessctl set "35%"
+    brightnessctl set "5%"
     hyprctl hyprsunset gamma 75
     hyprctl hyprsunset temperature 2500 
     notify-send -t 1000 "gone blue"
@@ -11,6 +11,6 @@ else
     touch "$STATE_FILE"
     brightnessctl set "70%"
     hyprctl hyprsunset gamma 100
-    hyprctl hyprsunset temperature 20000 
+    hyprctl hyprsunset temperature 6000 
     notify-send -t 1000 "blue gone"
 fi
