36c627ea0f10abcfde0779e1dc74808d584e9a70
Santosh Gyawali <isantoshgyawali@gmail.com>
2026-04-20 11:58:57 +0545

    scaling to 1.33 instead of 1.5

--- config/hypr/scripts/scale.sh
+++ config/hypr/scripts/scale.sh
@@ -2,10 +2,10 @@
 
 CURRENT=$(hyprctl monitors | grep "eDP-1" -A20 | grep scale | awk '{print $2}')
 
-if [ "$CURRENT" = "1.50" ]; then
+if [ "$CURRENT" = "1.33" ]; then
     notify-send -t 2000 "res: 1366x768 :: scale: 1"
     hyprctl keyword monitor "eDP-1,1366x768,0x0,1"
 else
-    notify-send -t 2000 "res: 1920x1080 :: scale: 1.5"
-    hyprctl keyword monitor "eDP-1,1920x1080@60,0x0,1.5"
+    notify-send -t 2000 "res: 1920x1080 :: scale: 1.33"
+    hyprctl keyword monitor "eDP-1,1920x1080@60,0x0,1.33"
 fi
